38ed41dea677e2da68d583f925e0608c1f2ec351
Osberg <ga79moz@gerland2.t30.ph.tum.de>
2014-04-27 14:58:20 +0200

    set metajob as the single control point for everything

--- carpark_metajob.sh
+++ carpark_metajob.sh
@@ -1,5 +1,7 @@
 #--- carpark_metajob.sh -a script that sets up the run for the phasespace scan and then submits it.
-# ---last updated on  Sun Apr 27 14:21:11 CEST 2014  by  bren  at location  , bren-Desktop
+# ---last updated on  Sun Apr 27 14:57:27 CEST 2014  by  ga79moz  at location  TUM , lagrange2
+
+#  changes from  Sun Apr 27 14:57:27 CEST 2014 : set this file to be the main control -also determines input for void_numerics.in before copying it over to the WORKDIR
 
 #  changes from  Sun Apr 27 14:21:11 CEST 2014 : fixed the sub_tail file with the correct modulo operator (-1)
 
@@ -7,7 +9,32 @@
 
 #---------------------------------------------------------------------------------------------------------------------------
 
-eps_array_file="/home/t30/ger/ga79moz/grad_research_phd/project/Nucl/void_numerics/array_eps_a10.txt"
+NGtype=LNG
+
+Llim=200
+t0="0.0000000001"
+tf="80"
+a="10"  
+
+should_plot_Vdist_v_t="0"
+should_plot_rhos="1"
+should_check_neg="1"
+
+should_import_IC="0"
+should_export_IC="1"
+t_export="20"
+
+BZcond="boltzmann_on_add"
+parity_check="88855888"
+opath="./"
+
+
+job_sub_script=carpark_jobsub_TUM.sh
+WORKDIR=./void_EQ_output_on_space/job_phasespace_a-${a}_output_${NGtype}/
+
+#----------------------------------------------------------------------------------------
+
+eps_array_file="/home/t30/ger/ga79moz/grad_research_phd/project/Nucl/void_numerics/array_eps_a"${a}".txt"
 
 if [ -f ${eps_array_file} ];
 then
@@ -17,7 +44,7 @@ else
    exit 
 fi
 
-muN_array_file="/home/t30/ger/ga79moz/grad_research_phd/project/Nucl/void_numerics/array_muN_a10.txt"
+muN_array_file="/home/t30/ger/ga79moz/grad_research_phd/project/Nucl/void_numerics/array_muN_a"${a}".txt"
 
 if [ -f ${muN_array_file} ];
 then
@@ -35,14 +62,20 @@ num_tasks=$(( ${num_eps}*${num_mus} ))
 
 echo "num_eps=" ${num_eps} ", num_mus=" ${num_mus}
 echo "preparing to submit num_tasks=" ${num_tasks}
-a=10
-NGtype=LNG
 
+#-----------------SET UP THE C++ INPUT FILE ---------------------------
+echo $Llim    >  void_numerics.in
+echo $t0 $tf  >> void_numerics.in
+echo $a       >> void_numerics.in
 
-job_sub_script=carpark_jobsub_TUM.sh
-WORKDIR=./void_EQ_output_on_space/job_phasespace_a-${a}_output_${NGtype}/
+echo $should_plot_Vdist_v_t  $should_plot_rhos $should_check_neg >> void_numerics.in
+echo $should_import_IC   $should_export_IC  $t_export            >> void_numerics.in
 
-#--------------------------------------------------------------------
+echo  $BZflag                        >>  void_numerics.in
+echo  $parity_check                  >>  void_numerics.in
+echo  $opath                         >>  void_numerics.in
+
+#---------------------------------------------------------------------
 
 # create a local directory for this job only
 if [ ! -e $WORKDIR ]; then
